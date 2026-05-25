4e25e549fda934e3ac95d46cdbddc26c7d622dd1
Mariia Krasnokutska <mariiakrasnokutska@Mariias-MacBook-Air.local>
2026-05-25 12:08:05 +0200

    Added remove dish function

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ add_dish("кава капучіно", 120)
 add_dish("кава еспресо", 80)
 
 show_menu()
+
+def remove_dish(name):
+    global menu
+    menu = [dish for dish in menu if dish["name"] != name]
+
+
+remove_dish("кава капучіно")
+
+show_menu()
+
